0e26f050accc22a2b9b1f723f85fc4386d7035d2
Takeda-Forever <85308264+Takeda-Forever@users.noreply.github.com>
2024-02-23 00:34:26 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+
+std::string encript(std::string w);
+std::string decript(std::string w);
+
+void unit_test() 
+{
+	std::vector < std::string> w =
+	{
+		"Bauka","Bauka0000","Bauka1234","Bauka9876",
+		"Kabuk","Kabuk0000","Kabuk1234","Kabuk9876",
+		"Tupat","Tupat0000","Tupat1234","Tupat9876",
+		"Shaka","Shaka0000","Shaka1234","Shaka9876",
+		"0123456789", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	};
+	std::vector<std::string> dec;
+	std::vector<std::string> enc;
+
+	for (std::string wd : w)
+		dec.push_back(encript(wd));
+
+	for (std::string wd : dec)
+		enc.push_back(decript(wd));
+
+	std::cout << "Words to Decript:\n";
+
+	for (int i = 1; i <= w.size(); i++)
+	{
+		std::cout << w.at(i-1) << ',';
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "After Decript:\n";
+
+	for (int i = 1; i <= dec.size(); i++)
+	{
+		std::cout  << dec.at(i-1) << ',';
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "After Encript:\n";
+
+	for (int i = 1; i <= enc.size(); i++)
+	{
+		std::cout  << enc.at(i-1) << ',';
+	}
+	
+}
+
+int main() 
+{
+	unit_test();
+}
+
+
+std::string encript(std::string w)
+{
+	std::string result;
+
+	for (char c : w)
+	{
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) // если это буква
+		{
+			if (c >= 'a' && c <= 'z') // если это маленькая буква
+				c = 'a' + (c - 'a' + 3) % 26;
+			else // иначе это большая буква
+				c = 'A' + (c - 'A' + 3) % 26;
+		}
+		else if (c >= '0' && c <= '9') // если это цифра
+			c = '0' + (c - '0' + 3) % 10;
+
+		result += c;
+	}
+
+	return result;
+}
+
+std::string decript(std::string w)
+{
+	std::string result;
+
+	for (char c : w)
+	{
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) // если это буква
+		{
+			if (c >= 'a' && c <= 'z') // если это маленькая буква
+				c = 'a' + (c - 'a' + 23) % 26;
+			else // иначе это большая буква
+				c = 'A' + (c - 'A' + 23) % 26;
+		}
+		else if (c >= '0' && c <= '9') // если это цифра
+			c = '0' + (c - '0' + 7) % 10;
+
+		result += c;
+	}
+
+	return result;
+}
